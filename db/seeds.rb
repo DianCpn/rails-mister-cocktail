7d309c1208f83dfbf8ab997e9c90d12d027749fb
Diana Capunay <diana.capunay-jara@hotmail.com>
2019-11-14 16:41:24 +0100

    mister cocktail code done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 puts 'Cleaning database...'
 Ingredient.destroy_all
+Cocktail.destroy_all
+Dose.destroy_all
 
 puts 'Creating ingredients...'
 
@@ -8,7 +10,7 @@ Ingredient.create(name: "pisco")
 Ingredient.create(name: "egg white")
 Ingredient.create(name: "liquid cane sugar")
 Ingredient.create(name: "ice")
-Ingredient.create(name: "bitter angostura")
+Ingredient.create(name: "Bitter Angostura")
 Ingredient.create(name: "cinnamon powder")
-Ingredient.create(name: "ginger ale")
+Ingredient.create(name: "Ginger Ale")
 puts 'Finished!'
